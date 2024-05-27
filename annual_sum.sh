934a760be041f61b0f0ddb0ce4e419c6f98b0d33
Stephen Huysman <shuysman@gmail.com>
2024-05-26 19:30:41 -0600

    fix up annual sum script

--- annual_sum.sh
+++ annual_sum.sh
@@ -1,31 +1,27 @@
-in_dir="/home/steve/out/collated/"
-out_dir="/home/steve/out/"
+#!/bin/bash
+set -euxo pipefail
 
-deficit_files=$(find $in_dir -type f -name "Deficit*" -print)
+in_dir="~/out/collated/"
+out_dir="~/out/sums/"
 
-parallel -j 12 cdo yearsum {} {}_sum.nc ::: $deficit_files
+vars="AET Deficit"
+models="bcc-csm1-1-m bcc-csm1-1 BNU-ESM CanESM2 CNRM-CM5 CSIRO-Mk3-6-0 GFDL-ESM2G GFDL-ESM2M HadGEM2-CC365 HadGEM2-ES365 inmcm4 IPSL-CM5A-LR IPSL-CM5A-MR IPSL-CM5B-LR MIROC5 MIROC-ESM-CHEM MIROC-ESM MRI-CGCM3 NorESM1-M"
+scenarios="rcp45 rcp85"
 
-out_files=$(find $in_dir -type f -name "Deficit*_sum.nc" -print)
+#models="historical"
+#scenario="gridmet"
 
-cdo mergetime $out_files $out_dir/Deficit_annual_sum_burroughs.nc
+calc_annual_sum () {
+    model=$1
+    scenario=$2
+    var=$3
 
-aet_files=$(find $in_dir -type f -name "AET*" -print)
+    wb_files=$(find $in_dir -type f -name "${model}_${scenario}_${var}_*_burroughs.nc" -print)
+    
+    parallel -j 12 cdo yearsum {} {}_sum.nc ::: $wb_files
+    out_files=$(find $in_dir -type f -name "${model}_${scenario}_${var}*_sum.nc" -print)
 
-parallel -j 12 cdo yearsum {} {}_sum.nc ::: $aet_files
+    cdo mergetime $out_files "${out_dir}/${model}_${scenario}_${var}_annual_sum_burroughs.nc"
+}
 
-out_files=$(find $in_dir -type f -name "AET*_sum.nc" -print)
-
-cdo mergetime $out_files $out_dir/AET_annual_sum_burroughs.nc
-
-
-### accumswe is generated for each pixel at the DEM scale, but is
-### calculated from variables only derived from larger grids
-### such as 4km climate and t50.
-### Therefore, it's not very interesting to look at at the meter scale
-# accumswe_files=$(find $in_dir -type f -name "accumswe*" -print)
-
-# parallel -j 12 cdo -O yearmax {} {}_sum.nc ::: $accumswe_files
-
-# out_files=$(find $in_dir -type f -name "accumswe*_sum.nc" -print)
-
-# cdo -O mergetime $out_files $out_dir/accumswe_annual_max_burroughs.nc
+parallel -j 12 calc_annual_sum {} ::: $vars ::: $models ::: $scenarios
